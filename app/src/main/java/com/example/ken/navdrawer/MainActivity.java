37a7e5f7e73d79f52f7210dc07b8b95669616d3b
ken <kennduatiwanjiku@gmail.com>
2017-10-31 11:19:08 +0300

    Web View Updated

--- app/src/main/java/com/example/ken/navdrawer/MainActivity.java
+++ app/src/main/java/com/example/ken/navdrawer/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.mindorks.placeholderview.PlaceHolderView;
 
@@ -21,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawer = (DrawerLayout)findViewById(R.id.drawerLayout);
-        mDrawerView = (PlaceHolderView)findViewById(R.id.drawerView);
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerView = (PlaceHolderView) findViewById(R.id.drawerView);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mGalleryView = (PlaceHolderView) findViewById(R.id.galleryView);
         setupDrawer();
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.youtube.com/watch?v=b7iZ-LDh01E");
+        myWebView.setHorizontalScrollBarEnabled(false);
+        myWebView.getSettings().setSupportZoom(true);
+        myWebView.getSettings().setDisplayZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(true);
     }
 
     private void setupDrawer() {
@@ -41,13 +51,18 @@ public class MainActivity extends AppCompatActivity {
                 .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_LOGOUT));
 
 
-        ActionBarDrawerToggle  drawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open_drawer, R.string.close_drawer){
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open_drawer, R.string.close_drawer) {
+
+
             @Override
             public void onDrawerOpened(View drawerView) {
+
                 super.onDrawerOpened(drawerView);
             }
+
             @Override
             public void onDrawerClosed(View drawerView) {
+
                 super.onDrawerClosed(drawerView);
             }
         };
